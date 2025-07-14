af3342d2a3302ba3d0170cc952c9726102c9b477
Anton Bowden <bowden.anton@gmail.com>
2025-07-14 12:07:30 +0100

    use command line argument

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words, count_characters, make_sorted_list
+import sys
 
 def get_book_text(filepath):
   with open(filepath) as f:
@@ -10,13 +11,16 @@ def print_report(book_path, num_words, sorted_list):
   print("----------- Word Count ----------")
   print(f"Found {num_words} total words")
   print("--------- Character Count -------")
-  
+
   for item in sorted_list:
     if item["char"].isalpha():
       print(f"{item["char"]}: {item["count"]}")
 
 def main():
-  book_path = "./books/frankenstein.txt"
+  if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+  book_path = sys.argv[1]
   book_text = get_book_text(book_path)
   num_words = count_words(book_text)
   chars_dict = count_characters(book_text)
