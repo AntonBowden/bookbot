3682acdbc9af785f325fa12b46ea80bd6865c32a
Anton Bowden <bowden.anton@gmail.com>
2025-07-14 11:58:29 +0100

    print report

--- main.py
+++ main.py
@@ -4,11 +4,23 @@ def get_book_text(filepath):
   with open(filepath) as f:
     return f.read()
 
+def print_report(book_path, num_words, sorted_list):
+  print("============ BOOKBOT ============")
+  print(f"Analyzing book found at {book_path}...")
+  print("----------- Word Count ----------")
+  print(f"Found {num_words} total words")
+  print("--------- Character Count -------")
+  
+  for item in sorted_list:
+    if item["char"].isalpha():
+      print(f"{item["char"]}: {item["count"]}")
+
 def main():
   book_path = "./books/frankenstein.txt"
   book_text = get_book_text(book_path)
   num_words = count_words(book_text)
   chars_dict = count_characters(book_text)
   sorted_list = make_sorted_list(chars_dict)
+  print_report(book_path, num_words, sorted_list)
 
 main()
